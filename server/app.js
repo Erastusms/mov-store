8b983de0100c12ba8f2f1230579c316ad720bedb
Erastus Mauliate <emauliate17@gmail.com>
2021-08-19 16:25:13 +0700

    initial commit

---
+++ server/app.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require('cors')
+const path = require('path')
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(cors())
+// app.use('/assets/images', express.static(path.join(__dirname,'/assets/images')))
+
+// const routes = require('./routes');
+// app.use(routes)
+
+app.listen(port,()=>{
+    console.log('App is running in port: ',port)
+})
